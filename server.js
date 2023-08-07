d6e15f3f57be8e7f75502ad7ba9d1b927c59d2a2
Onur Gonullu <21060336@stu.omu.edu.tr>
2023-08-07 03:24:35 +0300

    dasfa

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+
+dotenv.config({ path: './config/env/config.env' });
+
+
+app.listen(process.env.PORT, ()=> {
+    console.log('Server started successfully on port : ' + process.dotenv.PORT);
+})
+
+
